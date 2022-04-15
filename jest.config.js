de77bcc2c692cd2aa6a2e04302c0aab119c87e11
Lucas Salomao <lucaas.afonso@hotmail.com>
2022-04-14 22:39:17 -0300

    Adding jest to project

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  collectCoverage: true, // Indicates whether the coverage information should be collected while executing the test
+
+  coverageDirectory: 'coverage', // The directory where Jest should output its coverage files
+
+  coverageProvider: 'v8', // Indicates which provider should be used to instrument code for coverage
+
+  testEnviornment: 'node'
+}
